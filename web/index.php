25986a5a8faa6a11f11d81da3ddc1433cf4935b4
Christan Grant <christangrant@gmail.com>
2012-06-24 01:18:14 -0400

    new package for the topic explorations

--- web/index.php
+++ web/index.php
@@ -150,7 +150,7 @@ pg_free_result($result);
 foreach($topicrows as $row) { 
 	$tid = $row['tid'];
 	print "\$('#topic-docs-$tid').on('show', function() {\n";
-	print "if ($('#topic-docs-$tid').is(':empty')) {\n";
+	print "\tif ($('#topic-docs-$tid').is(':empty')) {\n";
 	print "\t\$.ajax({\n";
 	print "\t\t\ttype: 'GET',\n";
 	print "\t\t\turl: 'http://neo.cise.ufl.edu/grisham/paper/web/query.php', \n";
@@ -169,6 +169,7 @@ foreach($topicrows as $row) {
 	print "\t\t\t}\n";
 	print "\t});";
 	print "}\n";
+	print "});";
 }
 ?>
 			});
