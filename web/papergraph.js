1ced5f86e08454cbc12ffe81663ea18a6a6f0614
Christan Grant <christangrant@gmail.com>
2012-06-26 02:39:57 -0400

    graph viz, new arrangement

--- web/papergraph.js
+++ web/papergraph.js
@@ -497,7 +497,7 @@ function initializeGraphExplorer() {
       style: 'bold'
     },
     //Add Tips
-    Tips: {
+    /*Tips: {
       enable: true,
       onShow: function(tip, node) {
         //count connections
@@ -507,7 +507,7 @@ function initializeGraphExplorer() {
         tip.innerHTML = "<div class=\"tip-title\">" + node.name + "</div>"
           + "<div class=\"tip-text\"><b>connections:</b> " + count + "</div>";
       }
-    },
+    }*/,
     // Add node events
     Events: {
       enable: true,
