530f2b8033592d1a2a93a5f295c11be40bc99f22
Christan Grant <christangrant@gmail.com>
2012-07-03 17:10:43 -0400

    Added API for returning citations of a paper

--- web/query.php
+++ web/query.php
@@ -376,7 +376,7 @@ else if(isset($_GET['q']) && isset($_GET['type']) && $_GET['type'] == "citations
 	$pid = rawurldecode($_GET['q']);
 
 	$query = "SELECT p.papertitle AS title, p.pubyear AS year, p.venue AS venue, p.abstract AS abstract ".
-				", (SELECT topic_distribution FROM theta WHERE t.pid = p.id LIMIT 1) AS topic ".
+				", (SELECT topic_distribution FROM theta AS t WHERE t.pid = p.id LIMIT 1) AS topic ".
 				"FROM reference AS r INNER JOIN paper AS p ON (r.citation = p.id) ".
 				"WHERE r.pid = $pid ";
 
