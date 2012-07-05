1dd06976e1ac1919d0b8384c80179e3242b21541
Christan Grant <christangrant@gmail.com>
2012-07-05 04:07:20 -0400

    use api to update paper graph

--- web/papergraph.js
+++ web/papergraph.js
@@ -244,10 +244,10 @@ function updatePaperGraph(paperid) {
 				showInfo("Paper " + paperid + " has " + res["rowcount"] + "citations.");
 			}
 
-			//myjson = $jit.util.extend(myjson, citations);
+			myjson = $jit.util.extend(myjson, citations);
 			//myjson = $jit.util.extend(myjson, testobj);
 
-			//myfd.loadJSON(myjson);
+			myfd.loadJSON(myjson);
 			//myfd.refresh();
 
 			myfd.computeIncremental({
